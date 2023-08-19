dac5d3e9000057a9f685fad1cfc644f0c342bddb
pawan <pawankumar7310@gmail.com>
2023-08-19 16:06:21 +0530

    add and remove implement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "../App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { add } from "../features/cartSlice";
+import { add, remove } from "../features/cartSlice";
 import { fetchProducts } from "../features/productSlice";
 import { STATUSES } from "../features/productSlice";
 
@@ -12,13 +12,24 @@ export default function Home() {
 
   console.log("dispatch product", product);
   console.log("navbar data-----", items);
-
+  let cartList = product?.map((data1) =>
+    items.some((item2) => item2?.id === data1?.id)
+  );
+  console.log("cartresult", cartList);
+  //  const result = data1.map((item1) =>
+  //   data2.some((item2) => item2.id === item1.id)
+  // );
+  // console.log(result);
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
   const handleAddProduct = (data) => {
     dispatch(add(data));
+  };
+  const handleRemoveProduct = (id) => {
+    dispatch(remove(id));
+    // console.log("product id", id);
   };
 
   if (status === STATUSES.LOADING) {
@@ -31,18 +42,28 @@ export default function Home() {
 
   return (
     <div className="product">
-      {product?.map((data) => (
+      {product?.map((data, i) => (
         <div key={data.id} className="prod text-center">
           <img className="image" src={data.image} alt="product-img" />
           <p>Price {data.price}</p>
           <div className="d-flex flex-column">
             <span>{data.category}</span>
-            <button
-              className="btn btn-success"
-              onClick={() => handleAddProduct(data)}
-            >
-              add to cart
-            </button>
+
+            {cartList[i] ? (
+              <button
+                className="btn btn-danger"
+                onClick={() => handleRemoveProduct(data.id)}
+              >
+                remove to Cart
+              </button>
+            ) : (
+              <button
+                className="btn btn-success"
+                onClick={() => handleAddProduct(data)}
+              >
+                add to Cart
+              </button>
+            )}
           </div>
         </div>
       ))}
